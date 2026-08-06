a5042ae05f2e555630cbab9c846b36092c59a5b3
Omirbay Akseleu <omirbajakseleu5@gmail.com>
2026-08-06 21:48:52 +0500

    Change RematBlock assignment to use nn.remat

--- model.py
+++ model.py
@@ -671,7 +671,7 @@ class FullHybridMoEModel(nn.Module):
         num_blocks = self.cfg.num_layers // self.cfg.layers_per_block
 
 
-        RematBlock = BlockDAR
+        RematBlock = nn.remat(BlockDAR, static_argnums=(6,))
 
         
         for block_idx in range(num_blocks):
