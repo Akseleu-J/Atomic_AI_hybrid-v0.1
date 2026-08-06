2ffd9d2c69ceffc7fd7897dc1c757c68427e9ecc
Omirbay Akseleu <omirbajakseleu5@gmail.com>
2026-08-06 18:31:34 +0500

    Remove remat from RematBlock assignment

--- model.py
+++ model.py
@@ -674,7 +674,7 @@ class FullHybridMoEModel(nn.Module):
         RematBlock = nn.remat(BlockDAR, static_argnums=(7,), policy='dots_saveable')
 
 
-        RematBlock = BlockDAR  # без remat
+        
         for block_idx in range(num_blocks):
             layer_idx_start = block_idx * self.cfg.layers_per_block
             x, history_blocks = RematBlock(
