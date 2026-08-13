00aecb6a923b5f158311e33d03d08abeae008065
Omirbay Akseleu <omirbajakseleu5@gmail.com>
2026-08-13 16:10:04 +0500

    Update kernel_b_solve.py

--- atomic_ops/kernel_b_solve.py
+++ atomic_ops/kernel_b_solve.py
@@ -9,7 +9,7 @@ import jax.numpy as jnp
 from jax.experimental import pallas as pl
 from jax.experimental.pallas import tpu as pltpu
 
-from kernel_a_scores import BT, BC, N_SUB
+from .kernel_a_scores import BT, BC, N_SUB
 
 _HIGHEST = jax.lax.Precision.HIGHEST
 
