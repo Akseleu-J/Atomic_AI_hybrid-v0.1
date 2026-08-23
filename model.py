672511598470ea4c9cb33470c21fd43db91617c3
Omirbay Akseleu <omirbajakseleu5@gmail.com>
2026-08-23 19:53:04 +0500

    Update model.py

--- model.py
+++ model.py
@@ -1001,7 +1001,7 @@ class FullHybridMoEModel(nn.Module):
 
         num_blocks = self.cfg.num_layers // self.cfg.layers_per_block
 
-        RematBlock = BlockDAR
+        RematBlock = nn.remat(BlockDAR)
 
         for block_idx in range(num_blocks):
             layer_idx_start = block_idx * self.cfg.layers_per_block
